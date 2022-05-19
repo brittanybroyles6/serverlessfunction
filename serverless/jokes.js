4ddfff90746ecbe2b91d99acc3ddc55d555858b1
Brittany <brittanybroyles6@gmail.com>
2022-05-20 04:47:48 +1000

    iguess

--- serverless/jokes.js
+++ serverless/jokes.js
@@ -1,1 +1,19 @@
-console.log('HelloWOrld');
+const fetch = require("node-fetch");
+
+exports.handler = async (event, context) => {
+  const url = "https://dadjokegenerator.com/";
+  try {
+    const jokeStream = await fetch(url, {
+      headers: {
+        Accept: "application/json"
+      }
+    });
+    const jsonJoke = await jokeStream.json();
+    return {
+      statusCode: 200,
+      body: jsonJoke
+    };
+  } catch (err) {
+    return { statusCode: 422, body: err.stack };
+  }
+};
